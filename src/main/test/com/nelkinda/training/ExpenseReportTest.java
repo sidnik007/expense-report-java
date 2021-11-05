a6bad9bb679ed616bb6c1c065c44285e18ba08e3
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2021-11-05 12:31:57 +0530

    Call the printReport function from test to get the expected data.

--- src/main/test/com/nelkinda/training/ExpenseReportTest.java
+++ src/main/test/com/nelkinda/training/ExpenseReportTest.java
@@ -2,10 +2,25 @@ package com.nelkinda.training;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static com.nelkinda.training.ExpenseType.*;
+
 public class ExpenseReportTest {
 
     @Test
     void printReportTest() {
 
+        List<Expense> expenses = List.of(
+                new Expense(DINNER, 1),
+                new Expense(DINNER, 5000),
+                new Expense(DINNER, 5001),
+                new Expense(BREAKFAST, 2),
+                new Expense(BREAKFAST, 1000),
+                new Expense(BREAKFAST, 1001),
+                new Expense(CAR_RENTAL, 4),
+                new Expense(CAR_RENTAL, 800)
+        );
+        new ExpenseReport().printReport(expenses);
     }
 }
